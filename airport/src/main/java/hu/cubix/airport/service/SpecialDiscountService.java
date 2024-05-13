ecd60bbab82dc2f366cf58ae2021e08b39f6b8e5
Kolos <karlovitz.kolos@gmail.com>
2024-05-13 15:51:03 +0200

    airport extended

--- airport/src/main/java/hu/cubix/airport/service/SpecialDiscountService.java
+++ airport/src/main/java/hu/cubix/airport/service/SpecialDiscountService.java
@@ -1,13 +1,31 @@
 package hu.cubix.airport.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import hu.cubix.airport.config.AirportConfigurationProperties;
+import hu.cubix.airport.config.AirportConfigurationProperties.Discount.Special;
 
 //@Service
 public class SpecialDiscountService implements DiscountService {
 
+//	@Value("${airport.discount.special.limit}")
+//	private int limit;
+//	@Value("${airport.discount.special.lowerPercent}")
+//	private int lowerPercent;
+//	@Value("${airport.discount.special.higherPercent}")
+//	private int higherPercent;
+	
+	@Autowired
+	private AirportConfigurationProperties config;
+		
+	
 	@Override
 	public int getDiscountPercent(int totalPrice) {
-		return totalPrice > 1000 ? 15 : 10;
+		Special specialConfig = config.getDiscount().getSpecial();
+// 		return totalPrice > limit ? higherPercent : lowerPercent;
+		return totalPrice > specialConfig.getLimit() ? specialConfig.getHigherPercent() : specialConfig.getLowerPercent();
 	}
 
 }
