62894cc2c1fdb139c02b733e4efd8be692a29511
Kolos <karlovitz.kolos@gmail.com>
2024-05-15 16:18:36 +0200

    airport with Thymeleaf

---
+++ airport/src/main/java/hu/cubix/airport/controller/AirportController.java
@@ -0,0 +1,42 @@
+package hu.cubix.airport.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import hu.cubix.airport.dto.AirportDto;
+
+@Controller
+public class AirportController {
+	
+	private List<AirportDto> airports = new ArrayList<>();
+
+	// Inicializáló blokk, Java alapszolgáltatás
+	{
+		airports.add(new AirportDto(1, "Budapest ferenc Liszt International", "BUD"));
+	}
+	
+
+	@GetMapping("/")
+	public String home(Map<String, Object> model) {
+		model.put("airports", airports);
+		model.put("newAirport", new AirportDto());
+		return "index";
+	}
+	
+	
+	@PostMapping("/airport")
+	public String createAirport(AirportDto airport) {
+		airports.add(airport);
+		
+		//model.put("airports", airports);
+		//model.put("newAirport", new AirportDto());
+		//return "index";
+		return "redirect:/";
+	}
+	
+}
